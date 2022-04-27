ecb7c2a0393ce3ab535ccdc262938e09564c6225
Tammy <tammycollinse@gmail.com>
2022-04-27 17:09:07 -0400

    commit tree ff811e7cdcad56153630084e4baa0d536c6f9c8b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title , presence: true
+    validates :content , length: { minimum: 250 }
+    validates :summary , length: { maximum: 250 }
+    validates :category , inclusion: { in: %w(fiction non-fiction)}
+    validate :clickbait_validation
+
+    def clickbait_validation
+        if title.downcase == "won't believe" || title.downcase == "secret" || title.downcase == "top " || title.downcase == "guess"
+        errors.add(:title, "sorry, this title is not clickbaity")
+        end
+    end
 end
